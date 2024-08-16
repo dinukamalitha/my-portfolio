978f4453fd22ed382ee1695ba2a86e7208ca810a
Dinuka Malitha <92653525+dinukamalitha@users.noreply.github.com>
2024-08-16 21:15:47 +0530

    Navbar Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+import Navbar from "./components/navabar/navbar.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App;
