14732e442ec40b950f218ebe7bbac0f99b4160ef
dinukamalitha <dmcoder01@gmail.com>
2024-08-27 11:42:44 +0530

    Tech Stack Modified

--- src/components/techstack/tehstack.jsx
+++ src/components/techstack/tehstack.jsx
@@ -1,47 +1,70 @@
 import technologies from "../../data/technologies.json";
-import  experiences from "../../data/expeiences.json";
 import {getImageUrl} from "../../utils.js";
 import styles from "./techstack.module.css";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from "@mui/material/Typography";
 
 export const Techstack = () => {
     return (
         <section id="tech stack" className={styles.container}>
             <h2 className={styles.title}>Tech Stack</h2>
-            <div className={styles.content}>
-                <div className={styles.technologies}>
-                    {technologies.map((technology, id) => {
-                        return (
-                            <div key={id} className={styles.technology}>
-                                <div className={styles.technologyImgContainer}>
-                                    <img src={getImageUrl(technology.imageSrc)} alt={technology.title}/>
-                                </div>
-                                <p>{technology.title}</p>
-                            </div>
-                        );
-                    })}
-                </div>
-                <ul className={styles.experiences}>
-                    {experiences.map((experience,id)=> {
-                        return(
-                            <li key={id} className={styles.experienceItem}>
-                                <img
-                                    src={getImageUrl(experience.imageSrc)}
-                                    alt={`${experience.organisation} logo`}
-                                />
-                                <div className={styles.experienceDetails}>
-                                    <h3>{`${experience.role}`}, {`${experience.organisation}`}</h3>
-                                    <p>{`${experience.startDate}`} - {`${experience.endDate}`}</p>
-                                    <ul>
-                                        {experience.workExperiences.map((workExperience,id) =>{
-                                            return <li key={id}>{workExperience}</li>
-                                        })}
-                                    </ul>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
+            <Box sx={{ flexGrow: 1, marginTop: '25px' }}>
+                <Grid container spacing={4}>
+                    <Grid item xs={4}>
+                        <Typography variant="h5" sx={{textAlign:'center', color: 'white'}} gutterBottom>
+                            Front-End
+                        </Typography>
+                        <div className={styles.technologiesCard}>
+                            {technologies.frontend.map((technology, id) => {
+                                return (
+                                    <div key={id} className={styles.technology}>
+                                        <div className={styles.technologyImgContainer}>
+                                            <img src={getImageUrl(technology.imageSrc)} alt={technology.title}/>
+                                        </div>
+                                        <p>{technology.title}</p>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography variant="h5" sx={{textAlign:'center', color: 'white'}} gutterBottom>
+                            Back-End
+                        </Typography>
+                        <div className={styles.technologiesCard}>
+                            {technologies.backend.map((technology, id) => {
+                                return (
+                                    <div key={id} className={styles.technology}>
+                                        <div className={styles.technologyImgContainer}>
+                                            <img src={getImageUrl(technology.imageSrc)} alt={technology.title}/>
+                                        </div>
+                                        <p>{technology.title}</p>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography variant="h5" sx={{textAlign:'center', color: 'white'}} gutterBottom>
+                            DBMS & Deployment
+                        </Typography>
+                        <div className={styles.technologiesCard}>
+                            {technologies.dbms.map((technology, id) => {
+                                return (
+                                    <div key={id} className={styles.technology}>
+                                        <div className={styles.technologyImgContainer}>
+                                            <img src={getImageUrl(technology.imageSrc)} alt={technology.title}/>
+                                        </div>
+                                        <p>{technology.title}</p>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </Grid>
+                </Grid>
+            </Box>
+
         </section>
     );
 }
